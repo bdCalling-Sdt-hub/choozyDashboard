9e91b877e8d9097bf6cce6e72faa7c3a96f6a9f5
unknown <jrtushar@gmail.com>
2024-11-13 04:26:14 -0800

    dashboard home status done

--- src/component/dashHome/Status.tsx
+++ src/component/dashHome/Status.tsx
@@ -5,8 +5,9 @@ import { FaArrowTrendUp } from "react-icons/fa6";
 import { SiPaypal } from "react-icons/si";
 import { FcComboChart } from "react-icons/fc";
 import './Style_dashboard.css'
+import { useGetDashHomeStatusApiQuery } from "../../redux/features/getDashHomeStatusApi";
 
-// import { useGetAllStatusApiQuery } from "../redux/Features/getAllStatusApi";
+
 
 // Define a type for the API response
 interface StatusAttributes {
@@ -49,15 +50,16 @@ const Status: React.FC = () => {
   const [selectedCard, setSelectedCard] = useState<number | null>(null);
   const [selectedValue, setSelectedValue] = useState<string | undefined>()
   // Uncomment the next line when using the actual API
-  // const { data, isSuccess, isError, isLoading } = useGetAllStatusApiQuery();
+  const { data, isSuccess, isError, isLoading } = useGetDashHomeStatusApiQuery();
+  console.log("status", data?.data?.activeUsersCount)
 
   // For mock data demonstration
   // Replace this with the actual API response when needed
 
   // Uncomment the following block if using the API response
-  // if (isLoading) {
-  //   return <Loading />;
-  // }
+  if (isLoading) {
+    return <p>Loadding...</p>;
+  }
   const handleCardClick = (cardIndex: number) => {
     setSelectedCard(cardIndex);
   };
@@ -65,6 +67,44 @@ const Status: React.FC = () => {
     setSelectedValue(value);
     console.log("Selected", value);
   }
+
+  const cardData = [
+    {
+      id: 1,
+      icon: <HiMiniUsers size={20} />,
+      value: data?.data?.activeUsersCount,
+      title: 'Active Users',
+      // description: '0.5k Increased than last 7 days',
+    },
+    {
+      id: 2,
+      icon: <SiPaypal size={20} />,
+      value: data?.data?.monthlyRevenue,
+      title: 'Transactions',
+      // description: '39k Increased than last 7 days',
+    },
+    {
+      id: 3,
+      icon: <FcComboChart size={20} />,
+      value: data?.data?.totalTransactions,
+      title: 'Revenues',
+      // description: '1.5k Increased than last 7 days',
+    },
+    {
+      id: 4,
+      icon: <FcComboChart size={20} />,
+      value: data?.data?.weeklyRevenue,
+      title: 'Revenues',
+      // description: '1.5k Increased than last 7 days',
+    },
+    {
+      id: 5,
+      icon: <FcComboChart size={20} />,
+      value: data?.data?.yearlyRevenue,
+      title: 'Revenues',
+      // description: '1.5k Increased than last 7 days',
+    },
+  ];
 
   const selectOptions = [
     {value: '1', label: 'week'}, 
@@ -87,11 +127,11 @@ const Status: React.FC = () => {
           />
         </div>
       </div>
-      <div className="grid grid-cols-3 w-[calc(100% -300px)] mt-[12px]">
+      <div className="grid grid-cols-5 w-[calc(100% -300px)] mt-[12px]">
       {cardData.map((card, index) => (
         <div
           key={card.id}
-          className={`2xl:w-[450px] xl:w-[320px] lg:w-[190px] w-[450px] h-[210px] px-[20px] py-[32px] flex justify-between items-center rounded-2xl cursor-pointer ${
+          className={`2xl:w-[280px] xl:w-[320px] lg:w-[190px] w-[450px] h-[210px] px-[20px] py-[32px] flex justify-center items-center rounded-2xl cursor-pointer ${
             selectedCard === index ? 'bg-[#02B5AA] text-[#E8EBF0]' : 'border border-[#E7E7E7]'
           }`}
           onClick={() => handleCardClick(index)} 
@@ -104,8 +144,8 @@ const Status: React.FC = () => {
               {card.icon}
             </div> 
             <div className="flex items-center gap-2">
-              <h1 className="text-secondary py-4 text-[34px] font-bold">{card.value}</h1>
-              <FaArrowTrendUp color={selectedCard === index ? "white" : "#28A745"} size={20} />
+              <h1 className="text-secondary py-4 text-[34px] font-bold">${card.value}</h1>
+              {/* <FaArrowTrendUp color={selectedCard === index ? "white" : "#28A745"} size={20} /> */}
             </div>
             <h1 className="text-[16px] font-bold">{card.title}</h1>
             <p>{card.description}</p>
